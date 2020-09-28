22387ebc6986eb9ffc56733afa7ae1f472669332
cristina1979 <70112542+cristina1979@users.noreply.github.com>
2020-09-28 00:04:12 -0300

    Create pagina3.jsx

---
+++ pagina3.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+export default () => {
+  return (
+    <>
+      <View>
+        <Text>Página 3</Text>
+        <Text>3</Text>
+      </View>
+    </>
+  )
+}
