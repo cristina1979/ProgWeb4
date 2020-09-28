f6a709924b4bcd3361ea6d76438b424da208490b
cristina1979 <70112542+cristina1979@users.noreply.github.com>
2020-09-28 00:03:43 -0300

    Create pagina2.jsx

---
+++ pagina2.jsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+
+export default () => {
+  return (
+    <>
+      <View>
+        <Text>Página 2</Text>
+        <Text>2</Text>
+      </View>
+    </>
+  )
+}
